314cdf87564571b512bb1ca0aa5556327bef7ed7
EugeneLem <eugene.lemaitre@gmail.com>
2017-12-28 14:23:34 +0100

    Update graphics.h
    Addition of print game fonction

--- source/graphics.h
+++ source/graphics.h
@@ -2,6 +2,10 @@
 #include <stdio.h>
 
 
+
 void configureSub();
 
 void configureMain();
+
+void printGameSub( int gameMap[] );
+/*  fill Vram C with the tiles value. Where GameState is an array 16*32 with the tiles number in basicTiles.png */
