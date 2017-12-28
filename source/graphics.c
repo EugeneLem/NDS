d73afa79fa5ba45bde0b2c91d68106750d18c957
EugeneLem <eugene.lemaitre@gmail.com>
2017-12-28 14:24:18 +0100

    Update graphics.c
    Addition of printGame fonction

--- source/graphics.c
+++ source/graphics.c
@@ -18,31 +18,25 @@ void configureSub(){
 
 	swiCopy(basicTilesTiles,&BG_TILE_RAM_SUB(1)[0],basicTilesTilesLen);
 	swiCopy(basicTilesPal,&BG_PALETTE_SUB[0],basicTilesPalLen/2);
-
-
-
-
-	int i,j;
-	for(i=0;i<32;i+=2){
-		for(j=0;j<32;j+=2){
-			BG_MAP_RAM_SUB(0)[32*j+i]=basicTilesMap[0] ;
-			BG_MAP_RAM_SUB(0)[32*j+i+1]=basicTilesMap[1];
-			BG_MAP_RAM_SUB(0)[32*(j+1)+i]=basicTilesMap[2];
-			BG_MAP_RAM_SUB(0)[32*(j+1)+i+1]=basicTilesMap[3];
-
-			BG_MAP_RAM_SUB(1)[32*j+i]=basicTilesMap[4] ;
-			BG_MAP_RAM_SUB(1)[32*j+i+1]=basicTilesMap[5];
-			BG_MAP_RAM_SUB(1)[32*(j+1)+i]=basicTilesMap[6];
-			BG_MAP_RAM_SUB(1)[32*(j+1)+i+1]=basicTilesMap[7];
-
-
-		}
-	}
-	BG_MAP_RAM_SUB(0)[23]=basicTilesMap[6];
-
 }
 
+void printGameSub( int gameMap[] ){ 	//0:fermer  1:end 2:9 3:8 4:7 5:8 6:6 7:5 8:4 9:3 10:2 11:1 12:0 13:
+	int row,column;
+	for(row=0;row<16;row++){
+		for(column=0;column<16;column++){
+			BG_MAP_RAM_SUB(0)[32*row*2+column*2]=basicTilesMap[4*gameMap[16*row+column]] ;
+			BG_MAP_RAM_SUB(0)[32*row*2+column*2+1]=basicTilesMap[4*gameMap[16*row+column]+1] ;
+			BG_MAP_RAM_SUB(0)[32*(row*2+1)+column*2]=basicTilesMap[4*gameMap[16*row+column]+2] ;
+			BG_MAP_RAM_SUB(0)[32*(row*2+1)+column*2+1]=basicTilesMap[4*gameMap[16*row+column]+3] ;
+
+			BG_MAP_RAM_SUB(1)[32*row*2+column*2]=basicTilesMap[4*gameMap[16*row+column+16*16]] ;
+			BG_MAP_RAM_SUB(1)[32*row*2+column*2+1]=basicTilesMap[4*gameMap[16*row+column+16*16]+1] ;
+			BG_MAP_RAM_SUB(1)[32*(row*2+1)+column*2]=basicTilesMap[4*gameMap[16*row+column+16*16]+2] ;
+			BG_MAP_RAM_SUB(1)[32*(row*2+1)+column*2+1]=basicTilesMap[4*gameMap[16*row+column+16*16]+3] ;
+		}
+	}
+}
+
+
 void configureMain(){
-
-
 }
